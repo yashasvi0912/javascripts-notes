3b987d1800211d5d9fd74aac2cfaa40b345a7de0
yashasvi0912 <sakshibelkhede09@gmail.com>
2025-08-07 18:25:12 +0530

    add input & output

---
+++ input_output/script.js
@@ -0,0 +1,18 @@
+let num1 = 10
+let num2 = 20
+
+//arithmatic oprators
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+// assignment oprators
+ let data = 10
+
+ console.log(data += 10) 
+ console.log(data -= 10) 
+ console.log(data *= 10) 
+ console.log(data /= 10) 
+ console.log(data %= 10)
