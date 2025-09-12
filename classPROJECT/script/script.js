1d0aaae0345cfff9b2e9d16f0f9582d4d4919a9f
yashasvi0912 <sakshibelkhede09@gmail.com>
2025-09-12 21:49:29 +0530

    added today cls assignment

--- classPROJECT/script/script.js
+++ classPROJECT/script/script.js
@@ -1,33 +1,124 @@
-let tasks = []
+let tasks = JSON.parse(localStorage.getItem("data")) || []
 
-const addBtn = document.querySelector("#add-notes-button")
-const popup = document.querySelector(".add-notes-pop-up")
-const closeBtn = document.querySelector("#close-pop-up")
-const form = document.querySelector("#task-form")
-const container = document.querySelector(".tasks-container")
+let add_notes_button = document.querySelector("#add-notes-button")
 
-addBtn.onclick = () => popup.classList.add("active")
-closeBtn.onclick = () => popup.classList.remove("active")
+let targetElement = document.querySelector(".add-notes-pop-up")
 
-form.onsubmit = e => {
-    e.preventDefault()
-    let { title, description } = e.target
-    if (!title.value || !description.value) return alert("Fill all fields!")
+let closeButton = document.querySelector('#close-pop-up')
 
-    tasks.push({
-        title: title.value,
-        description: description.value,
-        timeStamp: `T:${new Date().toLocaleTimeString()} D:${new Date().toLocaleDateString()}`
+let taskForm = document.querySelector("#task-form")
+
+let isEdit = false
+
+let TaskEditIndex = null
+
+let searchBar = document.querySelector("#search-bar-field")
+
+let addNotesFormContainer = document.querySelector(".add-notes-form")
+
+add_notes_button.addEventListener('click', (event) => {
+    targetElement.classList.add("active")
+})
+
+closeButton.addEventListener('click', () => {
+    targetElement.classList.remove("active")
+})
+
+addNotesFormContainer.addEventListener("mouseleave", () => {
+    document.getElementById("formSubmitButton").click()
+})
+
+
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    try {
+        if (isEdit && TaskEditIndex != null) {
+            tasks[TaskEditIndex].title = event.target["title"].value
+            tasks[TaskEditIndex].description = event.target["description"].value
+        } else {
+            if (!event.target["title"].value || !event.target["description"].value) {
+                throw ("empty fields !")
+            }
+            tasks.push({
+                title: event.target["title"].value,
+                description: event.target["description"].value,
+                timeStamp: `T: ${new Date().toLocaleTimeString()} D: ${new Date().toLocaleDateString()}`
+            })
+        }
+        event.target["title"].value = ""
+        event.target["description"].value = ""
+        closeButton.click()
+        displayTask(tasks)
+        SaveDataIntoLocalStorage(tasks)
+    } catch (err) {
+        console.log("please added task data before submitting ! : ", err)
+    }
+})
+
+function displayTask(arrayToBeDisplayed) {
+
+    if (arrayToBeDisplayed.length == 0) {
+        document.querySelector("#task-container-title").innerText = "No Data To Display !"
+    } else {
+        document.querySelector("#task-container-title").innerText = "Task Data !"
+    }
+
+    document.querySelector('.tasks-container').innerHTML = ""
+    arrayToBeDisplayed.forEach((task, index) => {
+        let singleTask = document.createElement("div")
+        singleTask.classList.value = "task border p-4"
+        singleTask.innerHTML = `
+                            <h4 class="title">${task.title}</h4>
+                            <p class="description">
+                                ${task.description}
+                            </p>
+                            <span class="timeStamp">${task.timeStamp}</span>
+                            <button onClick='deleteTask(${index})'>delete</button>
+                            <button onClick='editTask(${index})'>Edit</button>
+    `
+        document.querySelector('.tasks-container').appendChild(singleTask)
+    })
+}
+function deleteTask(deleteIndex) {
+    let confirmDelete = window.confirm(`do you really want to delete ${deleteIndex} element ?`)
+    if (confirmDelete) {
+        tasks = tasks.filter((task, index) => { return index != deleteIndex })
+        SaveDataIntoLocalStorage(tasks)
+        displayTask(tasks)
+    } else {
+        alert("delete cancelled !")
+    }
+}
+
+function editTask(editIndex) {
+    // open pop up
+    targetElement.classList.add("active")
+    // access the data for selected task
+    console.log(tasks[editIndex].title)
+    console.log(tasks[editIndex].description)
+    // put selected data into input fields
+    taskForm["title"].value = tasks[editIndex].title
+    taskForm["description"].value = tasks[editIndex].description
+    // edit variable set
+    isEdit = true
+    TaskEditIndex = editIndex
+}
+
+searchBar.addEventListener("change", (event) => {
+    console.log(event.target.value)
+    let searchString = event.target.value
+
+    let filteredTasksArray = tasks.filter((task, index) => {
+        if (task.title.includes(searchString)) {
+            return task
+        }
     })
 
-    container.innerHTML = tasks.map(t => `
-        <div class="task border p-4">
-            <h4>${t.title}</h4>
-            <p>${t.description}</p>
-            <span>${t.timeStamp}</span>
-        </div>
-    `).join("")
+    displayTask(filteredTasksArray)
+})
 
-    form.reset()
-    popup.classList.remove("active")
+function SaveDataIntoLocalStorage(data) {
+    localStorage.setItem("data", JSON.stringify(data))
 }
+
+displayTask(tasks)
